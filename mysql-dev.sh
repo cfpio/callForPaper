94e691662ff4eadb785ca7bd548ddb802ba28eb3
Alexandre Thomazo <alex@thomazo.info>
2017-02-18 18:57:07 +0100

    ref #152 : using utf8mb4 in mysql to support emoji

--- mysql-dev.sh
+++ mysql-dev.sh
@@ -8,5 +8,6 @@ docker run --name mysql-cfp-dev -d -p 3306:3306 \
   -e MYSQL_DATABASE=cfpdev \
   -e MYSQL_USER=cfpdev \
   -e MYSQL_PASSWORD=galettesaucisse \
-  mysql:5.7
+  mysql:5.7 \
+  --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
 
