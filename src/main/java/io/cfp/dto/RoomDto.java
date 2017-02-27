53f0f788156bb4bb005ef7a4bd1f11340689244f
Nicolas De Loof <nicolas.deloof@gmail.com>
2017-02-27 17:44:02 +0100

    DTO need a default constructor

--- src/main/java/io/cfp/dto/RoomDto.java
+++ src/main/java/io/cfp/dto/RoomDto.java
@@ -7,13 +7,30 @@ import io.cfp.entity.Room;
  */
 public class RoomDto {
 
-    public final int id;
-    public final String name;
+    private int id;
+    private String name;
+
+    public RoomDto() {
+    }
 
     public RoomDto(Room room) {
         this.id = room.getId();
         this.name = room.getName();
     }
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
