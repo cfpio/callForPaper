df9feeddf1cefce334c3d477f2a9a1fc842dc2ec
Sebastien Brousse <seb.brousse@gmail.com>
2017-05-01 19:09:49 +0200

    Bascule en mapper xml Exemple de sql conditionnel pour éviter trop de méthodes dédiés Ajout de tests (bouuu !)

--- src/main/java/io/cfp/mapper/EventMapper.java
+++ src/main/java/io/cfp/mapper/EventMapper.java
@@ -1,9 +1,8 @@
 package io.cfp.mapper;
 
 import io.cfp.model.Event;
-import org.apache.ibatis.annotations.Insert;
+import io.cfp.model.queries.EventQuery;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -13,18 +12,15 @@ import java.util.List;
 @Mapper
 public interface EventMapper {
 
-    @Select("select * from events where open=true")
     List<Event> findOpen();
 
-    @Select("select * from events e where date < current_date()")
     List<Event> findPassed();
 
-    @Select("select * from events e where exists (select * from roles r where r.event_id = e.id AND r.user_id = #{user})")
     List<Event> findByUser(int user);
 
-    @Select("select exists(select 1 from events where id=#{id})")
+    List<Event> findAll(EventQuery eventQuery);
+
     boolean exists(String id);
 
-    @Insert("insert into events (id, name) values (#{id}, #{name})")
-    void insert(Event event);
+    int insert(Event event);
 }
---
+++ src/test/java/io/cfp/mapper/MapperTestApplication.java
@@ -0,0 +1,8 @@
+package io.cfp.mapper;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class MapperTestApplication {
+
+}
