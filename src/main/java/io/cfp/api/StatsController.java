19c890100f079504159adad690a22bd7acd4cf8a
Nicolas De Loof <nicolas.deloof@gmail.com>
2017-05-06 13:57:20 +0200

    /stats/meter available to speakers

--- src/main/java/io/cfp/api/StatsController.java
+++ src/main/java/io/cfp/api/StatsController.java
@@ -1,25 +1,29 @@
 package io.cfp.api;
 
 import io.cfp.domain.admin.meter.AdminMeter;
+import io.cfp.dto.RestrictedMeter;
 import io.cfp.mapper.ProposalMapper;
 import io.cfp.model.Role;
+import io.cfp.model.User;
 import io.cfp.model.queries.ProposalQuery;
 import io.cfp.multitenant.TenantId;
 import io.cfp.repository.SubmissionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.Serializable;
+
 import static io.cfp.model.Proposal.State.*;
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
 
 @RestController
 @RequestMapping(value = { "/v1/stats", "/api/stats" }, produces = APPLICATION_JSON_UTF8_VALUE)
-@Secured({Role.REVIEWER, Role.ADMIN})
 public class StatsController {
 
     @Autowired
@@ -33,30 +37,41 @@ public class StatsController {
      */
     @GetMapping(value="/meter")
     @ResponseBody
+    @Secured(Role.AUTHENTICATED)
     @Transactional(readOnly = true)
-    public AdminMeter getMeter(@TenantId String event) {
-        AdminMeter meter = new AdminMeter();
-        meter.setSpeakers(submissions.countByEventId(event));
+    public Serializable getMeter(@AuthenticationPrincipal User user,
+                                 @TenantId String event) {
 
-        ProposalQuery totalQuery = new ProposalQuery().setEventId(event).addStates(CONFIRMED, ACCEPTED, REFUSED, BACKUP);
-        meter.setTalks(proposals.count(totalQuery));
+        if (user.hasRole(Role.REVIEWER)) {
+            AdminMeter meter = new AdminMeter();
+            meter.setSpeakers(submissions.countByEventId(event));
 
-        ProposalQuery draftQuery = new ProposalQuery().setEventId(event).addStates(DRAFT);
-        meter.setDrafts(proposals.count(draftQuery));
+            ProposalQuery totalQuery = new ProposalQuery().setEventId(event).addStates(CONFIRMED, ACCEPTED, REFUSED, BACKUP);
+            meter.setTalks(proposals.count(totalQuery));
 
-        ProposalQuery submittedQuery = new ProposalQuery().setEventId(event).addStates(CONFIRMED);
-        meter.setSubmitted(proposals.count(submittedQuery));
+            ProposalQuery draftQuery = new ProposalQuery().setEventId(event).addStates(DRAFT);
+            meter.setDrafts(proposals.count(draftQuery));
 
-        ProposalQuery acceptedQuery = new ProposalQuery().setEventId(event).addStates(ACCEPTED);
-        meter.setAccepted(proposals.count(acceptedQuery));
+            ProposalQuery submittedQuery = new ProposalQuery().setEventId(event).addStates(CONFIRMED);
+            meter.setSubmitted(proposals.count(submittedQuery));
 
-        ProposalQuery rejectedQuery = new ProposalQuery().setEventId(event).addStates(REFUSED);
-        meter.setRejected(proposals.count(rejectedQuery));
+            ProposalQuery acceptedQuery = new ProposalQuery().setEventId(event).addStates(ACCEPTED);
+            meter.setAccepted(proposals.count(acceptedQuery));
 
-        ProposalQuery backupQuery = new ProposalQuery().setEventId(event).addStates(BACKUP);
-        meter.setBackup(proposals.count(backupQuery));
+            ProposalQuery rejectedQuery = new ProposalQuery().setEventId(event).addStates(REFUSED);
+            meter.setRejected(proposals.count(rejectedQuery));
 
-        return meter;
+            ProposalQuery backupQuery = new ProposalQuery().setEventId(event).addStates(BACKUP);
+            meter.setBackup(proposals.count(backupQuery));
+
+            return meter;
+        } else {
+            RestrictedMeter meter = new RestrictedMeter();
+
+            ProposalQuery mine = new ProposalQuery().setEventId(event).setUserId(user.getId()).addStates(CONFIRMED, ACCEPTED, REFUSED, BACKUP);
+            meter.setTalks(proposals.count(mine));
+            return meter;
+        }
     }
 
 }
