5a2ee78a6a4e6eff7791d3039321466cbfa7e935
Nicolas De Loof <nicolas.deloof@gmail.com>
2017-11-07 12:24:35 +0100

    use legacy controllers for missing rates API

--- src/main/java/io/cfp/controller/AdminRateController.java
+++ src/main/java/io/cfp/controller/AdminRateController.java
@@ -36,34 +36,16 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping(value = {"/v0/rates"}, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+@RequestMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 public class AdminRateController {
 
     @Autowired
     private RateAdminService rateService;
 
     /**
-     * Get all ratings
-     */
-    @RequestMapping(method = RequestMethod.GET)
-    @Secured(Role.ADMIN)
-    public List<RateAdmin> getRates() {
-        return rateService.getAll();
-    }
-
-    /**
-     * Delete all ratings
-     */
-    @RequestMapping(method = RequestMethod.DELETE)
-    @Secured(Role.ADMIN)
-    public void deleteRates() {
-        rateService.deleteAll();
-    }
-
-    /**
      * Add a new rating
      */
-    @RequestMapping(method=RequestMethod.POST)
+    @RequestMapping(value = "/api/rates", method=RequestMethod.POST)
     @Secured({Role.REVIEWER, Role.ADMIN})
     public RateAdmin postRate(@AuthenticationPrincipal io.cfp.model.User user, @Valid @RequestBody RateAdmin rate) throws NotFoundException {
         User currentUser = new User(user);
@@ -73,7 +55,7 @@ public class AdminRateController {
     /**
      * Edit a rating
      */
-    @RequestMapping(value= "/{rateId}", method=RequestMethod.PUT)
+    @RequestMapping(value = "/api/rates/{rateId}", method=RequestMethod.PUT)
     @Secured({Role.REVIEWER, Role.ADMIN})
     public RateAdmin putRate(@PathVariable int rateId, @Valid @RequestBody RateAdmin rate) {
         rate.setId(rateId);
@@ -84,7 +66,7 @@ public class AdminRateController {
     /**
      * Get a specific rating
      */
-    @RequestMapping(value= "/{rateId}", method= RequestMethod.GET)
+    @RequestMapping(value = "/api/rates/{rateId}", method= RequestMethod.GET)
     @Secured(Role.ADMIN)
     public RateAdmin getRate(@PathVariable int rateId) {
         return rateService.get(rateId);
@@ -95,7 +77,7 @@ public class AdminRateController {
      * @param userId
      * @return
      */
-    @RequestMapping(value= "/user/{userId}", method= RequestMethod.GET)
+    @RequestMapping(value = "/api/rates/user/{userId}", method= RequestMethod.GET)
     @Secured(Role.ADMIN)
     public List<RateAdmin> getRateByUserId(@PathVariable int userId) {
         return rateService.findForUser(userId);
@@ -104,7 +86,7 @@ public class AdminRateController {
     /**
      * Get all ratings for a given session
      */
-    @RequestMapping(value= "/proposals/{talkId}", method= RequestMethod.GET)
+    @RequestMapping(value = "/api/rates/proposals/{talkId}", method= RequestMethod.GET)
     @Secured(Role.ADMIN)
     public List<RateAdmin> getRatesByTalkId(@PathVariable int talkId) {
         return rateService.findForTalk(talkId);
@@ -113,29 +95,11 @@ public class AdminRateController {
     /**
      * Get rating for current user and a session
      */
-    @RequestMapping(value= "/proposals/{talkId}/me", method = RequestMethod.GET)
+    @RequestMapping(value = "/api/rates/proposals/{talkId}/me", method = RequestMethod.GET)
     @Secured({Role.REVIEWER, Role.ADMIN})
     public RateAdmin getRateByRowIdAndUserId(@AuthenticationPrincipal io.cfp.model.User user, @PathVariable int talkId) throws NotFoundException {
         int adminId = user.getId();
         return rateService.findForTalkAndAdmin(talkId, adminId);
     }
 
-    /**
-     * Delete specific rating
-     */
-    @RequestMapping(value= "/{rateId}", method= RequestMethod.DELETE)
-    @Secured(Role.ADMIN)
-    public void deleteRate(@PathVariable int rateId) {
-        rateService.delete(rateId);
-    }
-
-
-    /**
-     * Get Rates stats
-     */
-    @RequestMapping(value = "/stats", method = RequestMethod.GET)
-    @Secured(Role.ADMIN)
-    public Map<String, Long> getRateStats() {
-        return rateService.getRateByEmailUsers();
-    }
 }
